b820ceecab4efcd066b95e7827f2aceb03c9f8e0
yy <xxx>
2024-07-07 20:31:05 +0800

    b

--- oneplusone/index.039cd.js
+++ oneplusone/index.039cd.js
@@ -1,4 +1,4 @@
-System.register(["./application.js"], function (_export, _context) {
+System.register(["./application.a2dff.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
